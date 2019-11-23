baeb1bd5a5c3441c3c628622540079ee38d80d26
Mary Dick <maryclairedick@gmail.com>
2019-11-23 20:14:27 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,37 +3,45 @@ def create_an_empty_array
 end
 
 def create_an_array
-  rainbow = []
+  favorite_candy_flavors = ["blue rhaspberry", "orange", "watermelon", "strawberry"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  favorite_candy_flavors = ["green apple", "watermelon", "strawberry", "orange"]
+  favorite_candy_flavors << "lemon"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  favorite_candy_flavors = ["green apple", "watermelon", "strawberry", "orange"]
+  favorite_candy_flavors.unshift("blue rhaspberry")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  favorite_candy_flavors = ["green apple", "watermelon", "strawberry", "orange"]
+  favorite_candy_flavors.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  favorite_candy_flavors = ["green apple", "watermelon", "strawberry", "orange"]
+  favorite_candy_flavors.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  favorite_candy_flavors = ["green apple", "watermelon", "strawberry", "orange"]
+  favorite_candy_flavors[3]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  favorite_candy_flavors = ["green apple", "watermelon", "strawberry", "orange"]
+  favorite_candy_flavors[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  favorite_candy_flavors = ["green apple", "watermelon", "strawberry", "orange"]
+  favorite_candy_flavors[-1]
 end
 
 def update_element_from_index(array, index_number, element)
-
+  favorite_candy_flavors = ["green apple", "watermelon", "strawberry", "orange"]
+  favorite_candy_flavors[1] = "blue rhaspberry"
 end
