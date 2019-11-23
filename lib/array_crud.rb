682dde1dd74fe0e0e50e212077a29bce3a0256e7
Mary Dick <maryclairedick@gmail.com>
2019-11-23 20:34:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,17 +3,17 @@ def create_an_empty_array
 end
 
 def create_an_array
-  favorite_candy_flavors = ["Wow", "I", "am", "really"]
+  array = ["Wow", "I", "am", "really"]
 end
 
 def add_element_to_end_of_array(array, element)
-  favorite_candy_flavors = ["green apple", "watermelon", "strawberry", "orange"]
-  favorite_candy_flavors << "arrays!"
+  array = ["wow", "I", "am", "really", "learning"]
+  array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  favorite_candy_flavors = ["green apple", "watermelon", "strawberry", "orange"]
-  favorite_candy_flavors.unshift("wow")
+  array = ["wow", "I", "am", "really", "learning"]
+  array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
