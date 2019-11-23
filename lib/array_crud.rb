840f9ba28321eea64ab84b4d5d1139b069ee4471
Mary Dick <maryclairedick@gmail.com>
2019-11-23 20:19:14 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  favorite_candy_flavors = ["blue rhaspberry", "orange", "watermelon", "strawberry"]
+  favorite_candy_flavors = ["Wow", "I", "am", "really"]
 end
 
 def add_element_to_end_of_array(array, element)
