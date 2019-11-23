73f09f3e2f9b59c6e41c1a57d675e10c44f43e4a
Mary Dick <maryclairedick@gmail.com>
2019-11-23 20:29:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   favorite_candy_flavors = ["green apple", "watermelon", "strawberry", "orange"]
-  favorite_candy_flavors << "lemon"
+  favorite_candy_flavors << ["lemon"]
 end
 
 def add_element_to_start_of_array(array, element)
