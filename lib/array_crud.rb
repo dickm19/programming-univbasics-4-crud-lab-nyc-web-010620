74837d1a93f7cd2b03d66809c46487127c558bdd
Mary Dick <maryclairedick@gmail.com>
2019-11-23 20:38:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,36 +12,29 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  $array = ["wow", "I", "am", "really", "learning"]
-  array.unshift("wow")
+  $array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  array = ["wow", "I", "am", "really", "learning"]
-  array.pop
+  $array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  array = ["wow", "I", "am", "really", "learning"]
-  array.shift
+  $array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  array = ["wow", "I", "am", "really", "learning"]
-  array[3]
+  $array[3]
 end
 
 def retrieve_first_element_from_array(array)
-  favorite_candy_flavors = ["green apple", "watermelon", "strawberry", "orange"]
-  favorite_candy_flavors[0]
+  $array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  favorite_candy_flavors = ["green apple", "watermelon", "strawberry", "orange"]
-  favorite_candy_flavors[-1]
+  $array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
-  favorite_candy_flavors = ["green apple", "watermelon", "strawberry", "orange"]
-  favorite_candy_flavors[1] = "blue rhaspberry"
+  array[1] = "totally"
 end
